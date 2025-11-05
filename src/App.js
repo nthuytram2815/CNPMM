7e1e1c2d2b2370bc56805b2dd9b014fb8913edb8
NT-THTRM\nthuy <nthuytram2815@gmail.com>
2025-11-05 10:47:58 +0700

    BaiTap

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>CÔNG NGHỆ PHẦN MỀM MỚI</h1>
+      <p></p>
+      <h3>Thông tin cá nhân:</h3>
+      <h5>Họ và tên: Nguyễn Thùy Trâm</h5>
+      <p>MSSV: 22110249</p>
+      <p>Ngành: Công nghệ thông tin</p>
+      <p>Lớp: 22110CLST1B</p>
+      <p>Email: 22110249@student.hcmute.edu.vn</p>
+    </div> 
+  );
+}
+
+export default App;
